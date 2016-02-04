8cf7554b026390cda480268d1ad7c3be23a85002
Brian Sutton <basgolfer@gmail.com>
2016-02-03 21:52:34 -0500

    no message

---
+++ Brian Sutton Lab 1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brian_Sutton_Lab_1.Startup))]
+namespace Brian_Sutton_Lab_1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
